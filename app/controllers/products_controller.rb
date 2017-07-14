3013f519df8e6dae34c535cc00b589a629774ac2
Kat Gifford <katgiff2007@gmail.com>
2017-07-14 14:50:52 -0700

    add product show page(no model change)

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,6 +6,11 @@ class ProductsController < ApplicationController
     @order_item = current_order.order_items.new
   end
 
+  def show
+    @product = Product.find(params[:id])
+    @order_item = current_order.order_items.new
+  end
+
   def new
     @product = Product.new
   end
