4e60800f33b8b325271fec9583baa313510c064c
Kat Gifford <katgiff2007@gmail.com>
2017-07-16 10:45:13 -0700

    add product controller function for admin

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -28,6 +28,27 @@ class ProductsController < ApplicationController
     end
   end
 
+  def edit
+    @product = Product.find(params[:id])
+  end
+
+  def update
+    @product= Product.find(params[:id])
+    if @product.update(product_params)
+      flash[:notice] = "Product successfully updated"
+      redirect_to product_path(@product)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    product = Product.find(params[:id])
+    product.destroy
+    flash[:notice] = "Product successfully removed"
+    redirect_to products_path
+  end
+
   private
   def product_params
     params.require(:product).permit(:name, :description, :price, :image)
